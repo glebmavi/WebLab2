c97a7976e0f06a5c36525b1eae35f29e6b2a632d
Gleb Malikov Villegas ^-^ <44012947+glebmavi@users.noreply.github.com>
2023-10-08 23:57:57 +0300

    Logger in filter

--- src/main/java/co/glebmavi/webproglab2/filters/SessionLocaleFilter.java
+++ src/main/java/co/glebmavi/webproglab2/filters/SessionLocaleFilter.java
@@ -5,15 +5,19 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @WebFilter(filterName = "SessionLocaleFilter", urlPatterns = {"/*"})
 public class SessionLocaleFilter implements jakarta.servlet.Filter {
+
+    private static final Logger logger = Logger.getLogger(SessionLocaleFilter.class.getName());
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
 
         if (req.getParameter("sessionLocale") != null) {
+            logger.info("Setting session locale to " + req.getParameter("sessionLocale"));
             req.getSession().setAttribute("lang", req.getParameter("sessionLocale"));
         }
 
